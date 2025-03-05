426a3a95641f01b95f2ffc3b73732be6065f2f23
d3milade2005 <olakunledemilade11@gmail.com>
2025-03-05 12:54:05 +0100

    Changed /token to /login

--- src/auth.py
+++ src/auth.py
@@ -81,7 +81,7 @@ def register_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return db_user
 
 # User Login & Token Generation
-@auth_router.post("/token", response_model=schemas.Token)
+@auth_router.post("/login", response_model=schemas.Token)
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
